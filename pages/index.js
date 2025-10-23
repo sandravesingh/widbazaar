c5dd5bd0bf414b55343183bf711794a299a62b58
sandravesingh <singhsandrave@gmail.com>
2025-10-24 03:48:20 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>WidBazaar (MVP scaffold)</h1>
+      <p>Link-in-bio + Directory for Indian businesses.</p>
+    </main>
+  );
+}
